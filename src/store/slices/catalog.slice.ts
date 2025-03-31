c66e2860b599a1bef96747ddceb2cabf0fce6ae9
anto <ervinalvarezrodriguez@gmail.com>
2025-03-31 16:40:20 -0700

    deleted unused files

--- src/store/slices/catalog.slice.ts
+++
@@ -1,6 +0,0 @@
-import { StateCreator } from 'zustand'
-import { CatalogStoreType } from '../types/catalog.types'
-
-export const catalogSlice: StateCreator<CatalogStoreType> = set => ({
-  drinks: []
-})
